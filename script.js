8e5df8a2893c974bbd254439c671166153eade86
Restien <benmard8@outlook.de>
2023-10-07 02:08:01 +0200

    Erfahrungspunkte werden berechnet
    Je nach Werte und Veränderung dieser werden erfahrungspunkte neu berechnet, erfahrungspunkte können auch negativ sein (soll so bleiben)

--- script.js
+++ script.js
@@ -1,103 +1,70 @@
-// Funktion, um Erfahrungspunkte hinzuzufügen
-function addExperiencePoints() {
-    
+//elements
+var xp = document.getElementById("experience"); 
+var health = document.getElementById("health");
+var defense = document.getElementById("defense");
+var attack = document.getElementById("attack");
+var strength = document.getElementById("strength");
+var dexterity = document.getElementById("dexterity");
+var intelligence = document.getElementById("intelligence");
+var perception = document.getElementById("perception");
+var charisma = document.getElementById("charisma");
+
+//previous values
+var php = 10;
+var pdef = 0;
+var patt = 0;
+var pstr = 0;
+var pdex = 0;
+var pint = 0;
+var pcha = 0;
+var pper = 0;
+
+health.addEventListener("change", function() {
+    changed(php, health.value, 1);
+});
+defense.addEventListener("change", function() {
+    changed(pdef, defense.value, 4);
+});
+attack.addEventListener("change", function() {
+    changed(patt, attack.value, 4);
+});
+strength.addEventListener("change", function() {
+    changed(pstr, strength.value, 2);
+});
+dexterity.addEventListener("change", function() {
+    changed(pdex, dexterity.value, 2);
+});
+intelligence.addEventListener("change", function() {
+    changed(pint, intelligence.value, 2);
+});
+perception.addEventListener("change", function() {
+    changed(pper, perception.value, 2);
+});
+charisma.addEventListener("change", function() {
+    changed(pcha, charisma.value, 2);
+});
+
+function save()
+{
+	php = parseInt(health.value);
+    pdef = parseInt(defense.value);
+    patt = parseInt(attack.value);
+    pstr = parseInt(strength.value);
+    pdex = parseInt(dexterity.value);
+    pint = parseInt(intelligence.value);
+    pper = parseInt(perception.value);
+    pcha = parseInt(charisma.value);
+	console.log("Werte gespeichert");
 }
 
-// Funktion, um Erfahrungspunkte abzuziehen basierend auf den Werten
-function subtractExperiencePoints() {
-    var experience = parseInt(document.getElementById("experience").value);
-    var pointsToSubtract = 0;
-    
-    // Hier prüfen wir, welche Werte erhöht oder verringert werden und ziehen die Punkte entsprechend ab
-    var strength = parseInt(document.getElementById("strength").value);
-    if (strength > 0) {
-        pointsToSubtract += strength * 2;
-    }
-
-    var dexterity = parseInt(document.getElementById("dexterity").value);
-    if (dexterity > 0) {
-        pointsToSubtract += dexterity * 2;
-    }
-
-    var intelligence = parseInt(document.getElementById("intelligence").value);
-    if (intelligence > 0) {
-        pointsToSubtract += intelligence * 2;
-    }
-
-    var perception = parseInt(document.getElementById("perception").value);
-    if (perception > 0) {
-        pointsToSubtract += perception * 2;
-    }
-
-    var charisma = parseInt(document.getElementById("charisma").value);
-    if (charisma > 0) {
-        pointsToSubtract += charisma * 2;
-    }
-
-    experience -= pointsToSubtract;
-    // Verhindern, dass Erfahrungspunkte unter 0 gehen
-    experience = Math.max(0, experience);
-    document.getElementById("experience").value = experience;
+function changed(oldv, newv, changeval) {
+	console.log(oldv + " " + newv + " " + changeval);
+	
+	if(parseInt(newv) > parseInt(oldv)){
+		xp.value = parseInt(xp.value)-parseInt(changeval);
+	}
+	else if(parseInt(newv) < parseInt(oldv)){
+		xp.value = parseInt(xp.value)+parseInt(changeval);
+	}
 }
-
-// Funktion zum Speichern der Daten in einer Textdatei
-function saveToFile() {
-    var data = {
-        strength: document.getElementById("strength").value,
-        dexterity: document.getElementById("dexterity").value,
-        intelligence: document.getElementById("intelligence").value,
-        perception: document.getElementById("perception").value,
-        charisma: document.getElementById("charisma").value,
-        experience: document.getElementById("experience").value
-    };
-
-    var jsonData = JSON.stringify(data);
-    var blob = new Blob([jsonData], { type: "application/json" });
-
-    // Erzeuge einen Blob-URL für die Daten
-    var url = window.URL.createObjectURL(blob);
-
-    // Erzeuge einen <a> Link
-    var a = document.createElement("a");
-    a.href = url;
-    a.download = "character_data.json";
-
-    // Füge den Link zum Dokument hinzu
-    document.body.appendChild(a);
-
-    // Klicke auf den Link, um den Download auszulösen
-    a.click();
-
-    // Entferne den Link aus dem Dokument
-    document.body.removeChild(a);
-
-    // Entferne den Blob-URL, um Speicher zu sparen
-    window.URL.revokeObjectURL(url);
-}
-
-// Funktion zum Laden der Daten aus einer Textdatei
-function loadFromFile(event) {
-    var input = event.target;
-    var file = input.files[0];
-    var reader = new FileReader();
-
-    reader.onload = function() {
-        var jsonData = reader.result;
-        var data = JSON.parse(jsonData);
-
-        // Setze die Werte in die Eingabefelder zurück
-        document.getElementById("strength").value = data.strength;
-        document.getElementById("dexterity").value = data.dexterity;
-        document.getElementById("intelligence").value = data.intelligence;
-        document.getElementById("perception").value = data.perception;
-        document.getElementById("charisma").value = data.charisma;
-        document.getElementById("experience").value = data.experience;
-    };
-
-    reader.readAsText(file);
-}
-
-// Füge Event-Listener zu den Speichern- und Laden-Buttons hinzu
-document.getElementById("saveButton").addEventListener("click", saveToFile);
-document.getElementById("loadButton").addEventListener("change", loadFromFile);
-
+	
